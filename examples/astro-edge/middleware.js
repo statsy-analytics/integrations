9bef0cf09080dd80156a0c08d7bfc2982c850250
thom krupa <thom@bejamas.io>
2023-06-09 00:53:47 +0200

    fix: try js middleware

--- examples/astro-edge/middleware.js
+++ examples/astro-edge/middleware.js
@@ -1,6 +1,6 @@
 import { next } from "@vercel/edge";
 
-export async function middleware(request: Request) {
+export async function middleware(request) {
   return next({
     headers: { "x-from-middleware": "true" },
   });
