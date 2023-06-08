893b177a322fd12033f02694991fd58fb349d01a
thom krupa <thom@bejamas.io>
2023-06-09 00:29:25 +0200

    fix: change server to static runtime

--- examples/astro-edge/astro.config.mjs
+++ examples/astro-edge/astro.config.mjs
@@ -1,7 +1,3 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
-export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-});
+export default defineConfig({});
