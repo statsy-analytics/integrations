a497ea1017d790d7af65c113120717835c98126f
thom krupa <thom@bejamas.io>
2023-06-09 00:44:03 +0200

    feat: add vercel static adapter

--- examples/astro-edge/astro.config.mjs
+++ examples/astro-edge/astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
+import vercelStatic from "@astrojs/vercel/static";
 
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  adapter: vercelStatic(),
+});
