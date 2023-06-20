e9b1a0dcac775bb08214adc79cf1fe477926c508
thom krupa <thom@bejamas.io>
2023-06-20 17:05:42 +0200

    chore: update version

--- examples/server-side-tracking-deno/routes/_middleware.ts
+++ examples/server-side-tracking-deno/routes/_middleware.ts
@@ -1,5 +1,5 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
-import { trackPageview } from "https://esm.sh/@statsy/analytics";
+import { trackPageview } from "https://esm.sh/@statsy/analytics@0.1.4";
 
 export async function handler(request: Request, ctx: MiddlewareHandlerContext) {
   // Track the pageview
