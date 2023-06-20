5322edb96e77a4cb6297217f5e2829ac1b417ad8
thom krupa <thom@bejamas.io>
2023-06-20 16:10:12 +0200

    fix: pageview

--- examples/server-side-tracking-deno/routes/_middleware.ts
+++ examples/server-side-tracking-deno/routes/_middleware.ts
@@ -2,7 +2,7 @@ import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { trackPageview } from "https://esm.sh/@statsy/analytics";
 
 export async function handler(req: Request, ctx: MiddlewareHandlerContext) {
-  await trackPageview(req);
+  await trackPageview({ request: req });
 
   return ctx.next();
 }
