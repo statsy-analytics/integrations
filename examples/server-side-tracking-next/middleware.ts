b6a71c19dfc27f6ad98291a442092a2cb5ee3552
thom krupa <thom@bejamas.io>
2023-06-21 15:41:20 +0200

    feat: add next examples

---
+++ examples/server-side-tracking-next/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { trackPageview } from "@statsy/analytics";
+
+export async function middleware(request: NextRequest) {
+  await trackPageview({ request });
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
